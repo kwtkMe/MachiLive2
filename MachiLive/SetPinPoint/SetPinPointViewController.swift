92e35173a51698ce559f831bba3ad5b2f3e09f67
mychair.kke@gmail.com <mychair.kke@gmail.com>
2019-04-22 09:52:12 +0900

    edit

---
+++ MachiLive/SetPinPoint/SetPinPointViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SetPinPointViewController.swift
+//  MachiLive
+//
+//  Created by RIVER on 2019/04/22.
+//  Copyright © 2019 kwtkMe. All rights reserved.
+//
+
+import UIKit
+
+class SetPinPointViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
+
